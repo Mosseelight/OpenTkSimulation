d28208133e9af6c093d2be99993ec46fb587082a
Mosseelight <doctorphillip233@gmail.com>
2023-03-04 10:55:52 -0500

    change

--- src/Program.cs
+++ src/Program.cs
@@ -25,6 +25,30 @@ namespace OpenTkSim
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+
+
+            SwapBuffers();
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.4f, 0.8f, 0.9f, 1f);
+        }
+
+
+
+
+    
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
     }
 }
